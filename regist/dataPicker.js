2dfc6b0ff746568b057a89b8d54a53aee461e383
tianming <tianming@xiaokakeji.com>
2015-09-15 17:54:24 +0800

    基本信息录入 文件整理

--- regist/dataPicker.js
+++ regist/dataPicker.js
@@ -6,18 +6,18 @@
            //先给年下拉框赋内容
            var y  = new Date().getFullYear();
            for (var i = (y-30); i < (y+30); i++) //以今年为准，前30年，后30年
-                   document.reg_testdate.YYYY.options.add(new Option(" "+ i +" 年", i));
+                   document.dataPicker.YYYY.options.add(new Option(" "+ i +" 年", i));
 
            //赋月份的下拉框
            for (var i = 1; i < 13; i++)
-                   document.reg_testdate.MM.options.add(new Option(" " + i + " 月", i));
+                   document.dataPicker.MM.options.add(new Option(" " + i + " 月", i));
 
-           document.reg_testdate.YYYY.value = y;
-           document.reg_testdate.MM.value = new Date().getMonth() + 1;
+           document.dataPicker.YYYY.value = y;
+           document.dataPicker.MM.value = new Date().getMonth() + 1;
            var n = MonHead[new Date().getMonth()];
            if (new Date().getMonth() ==1 && IsPinYear(YYYYvalue)) n++;
                 writeDay(n); //赋日期下拉框Author:meizz
-           document.reg_testdate.DD.value = new Date().getDate();
+           document.dataPicker.DD.value = new Date().getDate();
    }
    if(document.attachEvent)
        window.attachEvent("onload", YYYYMMDDstart);
@@ -25,23 +25,23 @@
        window.addEventListener('load', YYYYMMDDstart, false);
    function YYYYDD(str) //年发生变化时日期发生变化(主要是判断闰平年)
    {
-           var MMvalue = document.reg_testdate.MM.options[document.reg_testdate.MM.selectedIndex].value;
-           if (MMvalue == ""){ var e = document.reg_testdate.DD; optionsClear(e); return;}
+           var MMvalue = document.dataPicker.MM.options[document.dataPicker.MM.selectedIndex].value;
+           if (MMvalue == ""){ var e = document.dataPicker.DD; optionsClear(e); return;}
            var n = MonHead[MMvalue - 1];
            if (MMvalue ==2 && IsPinYear(str)) n++;
                 writeDay(n)
    }
    function MMDD(str)   //月发生变化时日期联动
    {
-        var YYYYvalue = document.reg_testdate.YYYY.options[document.reg_testdate.YYYY.selectedIndex].value;
-        if (YYYYvalue == ""){ var e = document.reg_testdate.DD; optionsClear(e); return;}
+        var YYYYvalue = document.dataPicker.YYYY.options[document.dataPicker.YYYY.selectedIndex].value;
+        if (YYYYvalue == ""){ var e = document.dataPicker.DD; optionsClear(e); return;}
         var n = MonHead[str - 1];
         if (str ==2 && IsPinYear(YYYYvalue)) n++;
        writeDay(n)
    }
    function writeDay(n)   //据条件写日期的下拉框
    {
-           var e = document.reg_testdate.DD; optionsClear(e);
+           var e = document.dataPicker.DD; optionsClear(e);
            for (var i=1; i<(n+1); i++)
                 e.options.add(new Option(" "+ i + " 日", i));
    }
@@ -51,4 +51,3 @@
    {
         e.options.length = 1;
    }
-   
