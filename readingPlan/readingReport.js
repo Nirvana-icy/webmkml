b32c07f5b1e62c2cf9614e6b420e7958a36f0d0c
tianming <tianming@xiaokakeji.com>
2015-10-24 17:43:42 +0800

    完成课后评估 亲子版UI

--- readingPlan/readingReport.js
+++ readingPlan/readingReport.js
@@ -1,9 +1,9 @@
 $(function() {
   $(document).on("pageInit", function() {
-    $("#picker").picker({
+    $("#studentPicker").picker({
       toolbarTemplate: '<header class="bar bar-nav">\
       <button class="button button-link pull-right close-picker">确定</button>\
-      <h1 class="title">请选择你的手机</h1>\
+      <h1 class="title">请选择宝宝</h1>\
       </header>',
       cols: [
         {
@@ -12,23 +12,15 @@ $(function() {
         }
       ]
     });
-    $("#picker-name").picker({
+    $("#bookPicker").picker({
       toolbarTemplate: '<header class="bar bar-nav">\
       <button class="button button-link pull-right close-picker">确定</button>\
-      <h1 class="title">请选择称呼</h1>\
+      <h1 class="title">请选择绘本</h1>\
       </header>',
       cols: [
         {
           textAlign: 'center',
-          values: ['赵', '钱', '孙', '李', '周', '吴', '郑', '王']
-        },
-        {
-          textAlign: 'center',
-          values: ['杰伦', '磊', '明', '小鹏', '燕姿', '菲菲', 'Baby']
-        },
-        {
-          textAlign: 'center',
-          values: ['先生', '小姐']
+          values: ['iPhone 4', 'iPhone 4S', 'iPhone 5', 'iPhone 5S', 'iPhone 6', 'iPhone 6 Plus', 'iPad 2', 'iPad Retina', 'iPad Air', 'iPad mini', 'iPad mini 2', 'iPad mini 3']
         }
       ]
     });
